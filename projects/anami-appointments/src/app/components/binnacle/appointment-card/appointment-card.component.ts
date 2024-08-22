3a5be7188abef6475169d79f1f78c93b10ff4af9
Ikeda Harket <sebaaignacio111@gmail.com>
2024-08-22 14:01:23 -0400

    DEV: appoinment-card component style

---
+++ projects/anami-appointments/src/app/components/binnacle/appointment-card/appointment-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-appointment-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './appointment-card.component.html',
+})
+export class AppointmentCardComponent {
+
+}
--- projects/anami-appointments/src/app/pages/binnacle/history-patient/history-patient.component.ts
+++ projects/anami-appointments/src/app/pages/binnacle/history-patient/history-patient.component.ts
@@ -5,7 +5,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-history-patient',
   standalone: true,
-  imports: [ Components.TitleComponent, Components.DateCardComponent, CommonModule ],
+  imports: [ Components.TitleComponent, Components.DateCardComponent, Components.AppointmentCardComponent, CommonModule ],
   templateUrl: './history-patient.component.html',
 })
 export class HistoryPatientComponent {
