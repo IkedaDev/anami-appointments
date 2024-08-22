9a1a03770f8a6c4df5697bf1430538d2cb9bf658
Ikeda Harket <sebaaignacio111@gmail.com>
2024-08-22 14:57:15 -0400

    Se quita APP_BASE_HREF

--- projects/anami-appointments/src/app/app.config.ts
+++ projects/anami-appointments/src/app/app.config.ts
@@ -1,13 +1,11 @@
 import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideServiceWorker } from '@angular/service-worker';
-import { APP_BASE_HREF } from '@angular/common';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter( routes ), 
-    { provide: APP_BASE_HREF, useValue:'/pi/anami/agenda-hotel-tds' },
     provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
         registrationStrategy: 'registerWhenStable:30001'
